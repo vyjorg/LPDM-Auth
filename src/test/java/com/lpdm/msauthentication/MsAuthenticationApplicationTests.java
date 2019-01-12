1e96a539312f4b6817c80d0c87b4c4c9b11aee96
julien <juliencauwet@yahoo.fr>
2019-01-12 15:54:34 +0100

    removed springboottest annotation

---
+++ src/test/java/com/lpdm/msauthentication/MsAuthenticationApplicationTests.java
@@ -0,0 +1,17 @@
+package com.lpdm.msauthentication;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class MsAuthenticationApplicationTests {
+
+
+    @Test
+    public void test(){
+        Assert.assertTrue(1 == 1);
+    }
+}
